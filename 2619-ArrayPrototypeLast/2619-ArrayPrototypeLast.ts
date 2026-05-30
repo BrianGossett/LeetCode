f4cdcb4d6f784f4ca91e564e88b2bc44247ed055
Brian Gossett <84483732+BrianGossett@users.noreply.github.com>
2026-05-29 23:29:05 -0500

    File updated at 5/29/2026, 11:29:05 PM

--- 2619-ArrayPrototypeLast/2619-ArrayPrototypeLast.ts
+++ 2619-ArrayPrototypeLast/2619-ArrayPrototypeLast.ts
@@ -1,14 +1,15 @@
-// Last updated: 5/29/2026, 11:28:08 PM
+// Last updated: 5/29/2026, 11:29:05 PM
 1interface Array<T> {
 2    last(): T | -1;
 3}
 4
 5Array.prototype.last = function() {   
-6    return this.length === 0 ? -1 : this[this.length - 1]
-7};
-8
-9/**
-10 * const arr = [1, 2, 3];
-11 * arr.last(); // 3
-12 */
-13
+6    let len = this.length
+7    return len === 0 ? -1 : this[len - 1]
+8};
+9
+10/**
+11 * const arr = [1, 2, 3];
+12 * arr.last(); // 3
+13 */
+14
